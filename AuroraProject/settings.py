b3a5c49503b743a7777040530911a19e67d6cd62
Florin Hillebrand <florin.hillebrand@gmail.com>
2016-01-10 02:39:54 +0100

    django1.9: change to new style template settings.

--- AuroraProject/settings.py
+++ AuroraProject/settings.py
@@ -2,7 +2,6 @@ import os
 # Django settings for AuroraProject project.
 
 DEBUG = True
-TEMPLATE_DEBUG = DEBUG
 
 ADMINS = (
     # ('Your Name', 'your_email@example.com'),
@@ -58,6 +57,10 @@ MEDIA_ROOT = ''
 # Examples: "http://example.com/media/", "http://media.example.com/"
 MEDIA_URL = ''
 
+if DEBUG:
+    MEDIA_ROOT = os.path.curdir
+    MEDIA_URL = '/media'
+
 # Absolute path to the directory static files should be collected to.
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
@@ -67,6 +70,41 @@ STATIC_ROOT = ''
 # URL prefix for static files.
 # Example: "http://example.com/static/", "http://static.example.com/"
 STATIC_URL = '/static/'
+
+# A list containing the settings for all template engines to be used with
+# Django. Each item of the list is a dictionary containing the options for
+# an individual engine.
+#
+# - 'django.template.backends.django.DjangoTemplates'
+# - 'django.template.backends.jinja2.Jinja2'
+#
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'APP_DIRS': False,
+        'DIRS': [
+            os.path.join(os.path.dirname(__file__), '..', 'templates').replace('\\','/')
+        ],
+        'OPTIONS': {
+            'context_processors': [
+                "django.contrib.auth.context_processors.auth",
+                "django.core.context_processors.debug",
+                "django.core.context_processors.i18n",
+                "django.core.context_processors.media",
+                "django.core.context_processors.static",
+                "django.core.context_processors.tz",
+                "django.contrib.messages.context_processors.messages",
+                "AuroraProject.context_processor.general_context_processor",
+                "django.core.context_processors.request",
+            ],
+            'loaders': [
+                'django.template.loaders.filesystem.Loader',
+                'django.template.loaders.app_directories.Loader',
+            ],
+            'debug': True
+        }
+    },
+]
 
 # Additional locations of static files
 STATICFILES_DIRS = (
@@ -87,13 +125,6 @@ STATICFILES_FINDERS = (
 # Make this unique, and don't share it with anybody.
 SECRET_KEY = 'n@sn@dc_ayzqb2-u3cugqej7a_fj#^$b9$8h(m$r!us_oxz!d3'
 
-# List of callables that know how to import templates from various sources.
-TEMPLATE_LOADERS = (
-    'django.template.loaders.filesystem.Loader',
-    'django.template.loaders.app_directories.Loader',
-#     'django.template.loaders.eggs.Loader',
-)
-
 MIDDLEWARE_CLASSES = (
     'django.middleware.common.CommonMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
@@ -108,9 +139,6 @@ ROOT_URLCONF = 'AuroraProject.urls'
 
 # Python dotted path to the WSGI application used by Django's runserver.
 WSGI_APPLICATION = 'AuroraProject.wsgi.application'
-
-import os
-TEMPLATE_DIRS = (os.path.join(os.path.dirname(__file__), '..', 'templates').replace('\\','/'),)
 
 INSTALLED_APPS = (
     'django.contrib.auth',
@@ -176,18 +204,6 @@ LOGGING = {
 }
 
 LOGIN_URL = '/'
-
-TEMPLATE_CONTEXT_PROCESSORS = (
-    "django.contrib.auth.context_processors.auth",
-    "django.core.context_processors.debug",
-    "django.core.context_processors.i18n",
-    "django.core.context_processors.media",
-    "django.core.context_processors.static",
-    "django.core.context_processors.tz",
-    "django.contrib.messages.context_processors.messages",
-    "AuroraProject.context_processor.general_context_processor",
-    "django.core.context_processors.request",
-)
 
 ENDLESS_PAGINATION_PER_PAGE = (
     20
