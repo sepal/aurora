696ce758242b58c368cd17a0365e96d871019ecb
Florin Hillebrand <florin.hillebrand@gmail.com>
2016-01-11 04:57:30 +0100

    plagcheck: make sherlock work with strings and improve SQL queries.

--- Plagcheck/hashing/sherlock/sherlockmodule.c
+++ Plagcheck/hashing/sherlock/sherlockmodule.c
@@ -10,25 +10,44 @@ sherlock_signature(PyObject *self, PyObject *args)
     if (!PyArg_ParseTuple(args, "s", &filepath))
         return NULL;
 
-    ret_sig = signaturep(filepath);
+    ret_sig = signature_file(filepath);
 
     PyObject* list = PyList_New(0);
 
     int i = 0;
-    while (i < ret_sig->nval) {
+    while (i++ < ret_sig->nval) {
         PyObject *val = Py_BuildValue("l", ret_sig->val[i]);
         PyList_Append(list, val);
-        i++;
     }
 
-    assert(PyList_Check(list) == 1);
+    return list;
+}
+
+static PyObject *
+sherlock_signature_str(PyObject *self, PyObject *args)
+{
+    char *text;
+    Sig *ret_sig;
+
+    if (!PyArg_ParseTuple(args, "s", &text))
+        return NULL;
+
+    ret_sig = signature_str(text);
+
+    PyObject* list = PyList_New(0);
+
+    int i = 0;
+    while (i++ < ret_sig->nval) {
+        PyObject *val = Py_BuildValue("l", ret_sig->val[i]);
+        PyList_Append(list, val);
+    }
 
     return list;
 }
 
 static PyMethodDef module_methods[] = {
-    {"signature",  sherlock_signature, METH_VARARGS,
-     "Create a signature from file."},
+    {"signature",  sherlock_signature, METH_VARARGS, "Create a signature from file."},
+    {"signature_str", sherlock_signature_str, METH_VARARGS, "Create a signature from string."},
     {NULL, NULL, 0, NULL}        /* Sentinel */
 };
 
@@ -36,7 +55,7 @@ static struct PyModuleDef sherlock =
 {
     PyModuleDef_HEAD_INIT,
     "sherlock", /* name of module */
-    "Plagiarism detection algorithm",          /* module documentation, may be NULL */
+    "Plagiarism detection hashing algorithm. No multithreading support.",
     -1,          /* size of per-interpreter state of the module, or -1 if the module keeps state in global variables. */
     module_methods
 };
