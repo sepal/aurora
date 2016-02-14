b7d469a24abf5d34f7c1224593743e2cb9e790ef
ernads93 <ernadsehic@msn.com>
2016-02-14 13:15:46 +0100

    myReview done

--- Challenge/views.py
+++ Challenge/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render_to_response
 from django.template import RequestContext
 from django.contrib.auth.decorators import login_required
 from django.http import Http404
-
+import pprint
 from Course.models import Course
 
 from Stack.models import Stack, StackChallengeRelation
@@ -33,6 +33,21 @@ def create_context_myreview(request, course_short_title):
         user = RequestContext(request)['user']
 
         data['challenge'] = Challenge.objects.get(pk= request.GET.get('id'))
+        challenge = Challenge.objects.get(pk= request.GET.get('id'))
+
+        data['user_reviews'] = []
+        for review in challenge.get_reviews_written_by_user(user):
+           review_data = {}
+           review_data['review_id'] = review.id
+           review_data['review'] = review
+           review_data['appraisal'] = review.get_appraisal_display()
+           evaluation = ReviewEvaluation.objects.filter(review=review)
+           review_data['evaluation'] = ''
+           if evaluation:
+                review_data['evaluation'] = evaluation[0].appraisal
+           data['user_reviews'].append(review_data)
+
+
         data['course'] = Course.get_or_raise_404(course_short_title)
         return data
 
