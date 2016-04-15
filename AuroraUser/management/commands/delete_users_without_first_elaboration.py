9b2b07c061288fe351ec7feca939ba0e29cbe756
Rene Koller <r.koller@me.com>
2016-04-15 13:58:46 +0200

    added first version of delete users from course script

---
+++ AuroraUser/management/commands/delete_users_without_first_elaboration.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+from django.core.management.base import BaseCommand
+from django.contrib.auth.models import User, UserManager
+from AuroraUser.models import AuroraUser
+from Course.models import *
+from Challenge.models import Challenge
+from Elaboration.models import Elaboration
+
+
+class Command(BaseCommand):
+    help = 'Deletes all course_user relations for users without an Elaboration'
+
+    def handle(self, *args, **options):
+        delete_users_without_first_elaboration()
+
+
+def delete_users_without_first_elaboration():
+    # Users who have not submitted an elaboration for this
+    # challenges will be removed from the corresponding course
+    challenge_list = [1,2]
+
+    for challenge in Challenge.objects.filter(id__in=challenge_list):
+        # Get the course for this challenge
+        course = challenge.course
+        for user in AuroraUser.objects.all():
+            # Get all submitt
+            elaborations = Elaboration.objects.filter(challenge=challenge, user=user, submission_time__isnull=False)
+            if len(elaborations) > 0:
+                # User has submitted an elaboation for this challenge, dont't delete
+                continue
+            # User has not submitted an elaboration -> delete him from this course
+            CourseUserRelation.objects.filter(coures=course, user=user).delete()
