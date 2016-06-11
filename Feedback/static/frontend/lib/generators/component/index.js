f1a12ead0932e53b0a0a035c233a74cef3fc2f85
Sebastian Gilits <sep.gil@gmail.com>
2016-06-11 13:50:55 +0200

    Added generator for components.

---
+++ Feedback/static/frontend/lib/generators/component/index.js
@@ -0,0 +1,56 @@
+/**
+ * Component generator config.
+ */
+
+module.exports = {
+  description: 'Create a new component',
+  prompts: [
+    {
+      type: 'list',
+      name: 'type',
+      message: 'Select the component type',
+      default: 'Stateless function',
+      choices: () => ['Class', 'Stateless function']
+    },
+    {
+      type: 'input',
+      name: 'name',
+      message: 'What should be the name of new component?',
+      validate: value => {
+        if ((/.+/).test(value)) {
+          return true;
+        }
+
+        return 'A new is required';
+      }
+    },
+    {
+      type: 'confirm',
+      name: 'addStylesheet',
+      default: true,
+      message: 'Generate a stylesheet?'
+    }
+  ],
+  actions: data => {
+    var actions = [
+      {
+        type: 'add',
+        path: '../../app/components/{{properCase name}}/index.js',
+        templateFile: data.type == 'Class' ?  './component/class.js.hbs' : './component/stateless.js.hbs',
+        abortOnFail: true
+      }
+    ];
+
+    if (data.addStylesheet) {
+      actions.push({
+        type: 'add',
+        path: '../../app/components/{{properCase name}}/style.scss',
+        templateFile: './component/styles.scss.hbs'
+      })
+    }
+
+    console.log(actions);
+
+    return actions;
+  }
+};
