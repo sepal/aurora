427d16088f99182dfc3920a27ff8c6729fa4fccc
Florin Hillebrand <florin.hillebrand@gmail.com>
2016-06-21 11:24:45 +0200

    readd myreviews url.

--- Challenge/urls.py
+++ Challenge/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     url(r'^stack$', Challenge.views.stack, name='stack'),
     url(r'^challenge$', Challenge.views.challenge, name='challenge'),
     url(r'^$', Challenge.views.challenges, name='home'),
+    url(r'^myreviews$', Challenge.views.my_review, name='myreviews'),
 ]
