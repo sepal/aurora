4fcc06b6789a7e54114e481dd19978d28117f989
Sebastian Gilits <sep.gil@gmail.com>
2016-06-23 23:21:44 +0200

    Improved the feedback index js response and added issues to it.

--- Feedback/views.py
+++ Feedback/views.py
@@ -1,3 +1,4 @@
+import json
 from django.core import serializers
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
@@ -5,22 +6,26 @@ from django.views.decorators.csrf import csrf_exempt
 from django.http import HttpResponseRedirect
 from django.utils.safestring import mark_safe
 from Course.models import Course
-from .models import Lane
+from .models import Lane, Issue
 
 @login_required
 def index(request, course_short_title):
     """index renders a simple html page and adds the react frontend code."""
 
-    # Course and lanes are directly passed via javascript variables to the
-    # frontend code, since they are never changed by the app. This speeds up the
-    # rendering, since we don't have make seperate requests for the information.
+    # Pass some values directly as js variables, so that the client doesn't
+    # has to make additional requests.
     course = Course.get_or_raise_404(course_short_title)
-    lanes = serializers.serialize("json", Lane.objects.all().filter(hidden=False).order_by('order'), fields=('name'))
+    lanes = Lane.objects.all().filter(hidden=False).order_by('order')
+    lanes = list(map(lambda lane: {'id': lane.pk, 'name': lane.name}, lanes))
+
+    issues = Issue.objects.all().only('title', 'lane', 'type', 'title')
+    issues = list(map(lambda issue: {'id': issue.pk, 'title': issue.title, 'lane': issue.lane.id, 'type': issue.type}, issues))
 
     return render(
         request, 'Feedback/index.html',
         {
             'course': course,
-            'lanes': lanes,
+            'lanes': json.dumps(lanes),
+            'issues': json.dumps(issues)
         }
     )
