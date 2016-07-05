b9bf9a33e9031f651bc6b4bf4e5c46f1f5501fa9
Florin Hillebrand <florin.hillebrand@gmail.com>
2016-07-05 09:44:30 +0200

    use AuroraAuthenticationBackend for ModelBackend password verification.

--- middleware/AuroraAuthenticationBackend.py
+++ middleware/AuroraAuthenticationBackend.py
@@ -9,6 +9,19 @@ instead of the default Django user in request context.
 
 class AuroraAuthenticationBackend(ModelBackend):
 
+    def authenticate(self, username=None, password=None, **kwargs):
+        UserModel = AuroraUser
+        if username is None:
+            username = kwargs.get(UserModel.USERNAME_FIELD)
+        try:
+            user = AuroraUser.objects.get(matriculation_number=username)
+            if user.check_password(password):
+                return user
+        except UserModel.DoesNotExist:
+            # Run the default password hasher once to reduce the timing
+            # difference between an existing and a non-existing user (#20760).
+            UserModel().set_password(password)
+
     def get_user(self, user_id):
         try:
             return AuroraUser.objects.get(pk=user_id)
