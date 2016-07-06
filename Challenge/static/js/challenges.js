8f24a14fab64a4ff53649544c12a642743e28379
peterpur <purg@igw.tuwien.ac.at>
2016-07-06 14:33:19 +0200

    unlocking locked challenges

--- Challenge/static/js/challenges.js
+++ Challenge/static/js/challenges.js
@@ -72,7 +72,7 @@ $(function() {
 $(challenges_loaded);
 
 function challenges_loaded() {
-    $(".stack:not(.forbiddenfruit)").click(stack_clicked);
+    $(".stack").click(stack_clicked);
 }
 
 function stack_clicked(event) {
