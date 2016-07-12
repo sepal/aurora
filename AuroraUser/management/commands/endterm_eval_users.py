1ad133a5ace1710d5298fbecf4888008e5a939f5
Florin Hillebrand <florin.hillebrand@gmail.com>
2016-07-12 09:41:46 +0200

    add review karma to endterm_eval_users script.

--- AuroraUser/management/commands/endterm_eval_users.py
+++ AuroraUser/management/commands/endterm_eval_users.py
@@ -1,8 +1,17 @@
 from django.core.management.base import NoArgsCommand
 from AuroraUser.models import  AuroraUser
+from Course.models import Course, CourseUserRelation
 
 class Command(NoArgsCommand):
     def handle_noargs(self, **options):
+        courses = Course.objects.all()
+
+        header = ['matr_nr', 'nickname', 'first_name', 'last_name', 'study_code', 'last_activity', 'statement']
+        for course in courses:
+            header.append("review_karma " + course.short_title)
+
+        print("\t".join(header))
+
         for user in AuroraUser.objects.filter(is_staff=False):
             s = "\t".join(["{}"] * 7).format(user.matriculation_number, \
                                              user.nickname, \
@@ -10,6 +19,14 @@ class Command(NoArgsCommand):
                                              user.last_name, \
                                              user.study_code, \
                                              str(user.last_activity), \
-                                             user.statement)
+                                             user.statement, \
+                                             )
+            for course in courses:
+                try:
+                    karma = user.review_karma(course)
+                except CourseUserRelation.DoesNotExist:
+                    karma = '/'
+
+                s += "\t" + "{}".format(karma)
 
             print(s)
