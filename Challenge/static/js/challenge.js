2e1f3652fb106ba309d3862717d7128108fbd656
peterpur <peterpur@dhcp093.iguw.tuwien.ac.at>
2017-02-03 13:37:10 +0100

    better window (tab) titles for challenges + tasks

--- Challenge/static/js/challenge.js
+++ Challenge/static/js/challenge.js
@@ -1,6 +1,5 @@
 $(function () {
     $('#challenges-li').addClass('uRhere');
-    window.document.title = "Aurora: Challenges"
     $(".review_answer").each(function () {
         this.style.height = (this.scrollHeight+5)+'px';
     });
