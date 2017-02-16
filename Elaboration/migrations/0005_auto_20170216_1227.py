44a4b8367be7bba6b2189584b3cfd3f22664df95
Rene Koller <r.koller@me.com>
2017-02-16 13:20:43 +0100

    added migration

---
+++ Elaboration/migrations/0005_auto_20170216_1227.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('Elaboration', '0004_elaboration_most_helpful_other_user'),
+    ]
+
+    operations = [
+        migrations.AddField(
+            model_name='elaboration',
+            name='extra_review_question',
+            field=models.TextField(default=''),
+            preserve_default=True,
+        ),
+        migrations.AddField(
+            model_name='elaboration',
+            name='extra_review_question_answer',
+            field=models.TextField(default=''),
+            preserve_default=True,
+        ),
+    ]
