eb02f614a8c232ceb41dbf5743f217fa6905af5b
peterpur <peterpur@e253-180.eduroam.tuwien.ac.at>
2017-05-22 10:52:20 +0200

    Revert "small change on representation of found slides"
    This reverts commit 90edfc2a44edc6aa87dae859ecd20c22439686c4.

--- Slides/views.py
+++ Slides/views.py
@@ -191,7 +191,7 @@ def search(request, course_short_title=None):
 
         title = 'nothing found'
         if len(complete_list) != 0:
-            title = 'results found for "' + query + '":'
+            title = 'results found:'
 
     context = {
         "title": title,
