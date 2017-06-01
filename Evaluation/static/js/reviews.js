e8b753ba1a061b55e67416e66ee4ab7865132a61
peterpur <peterpur@flattie.local>
2017-06-01 23:45:18 +0200

    revising review layout. something is fishy here in denmark. hmmm...

--- Evaluation/static/js/reviews.js
+++ Evaluation/static/js/reviews.js
@@ -1,6 +1,6 @@
 $(function () {
     $(".review_answer").each(function () {
 	    if (this.value == "") {this.style.height = '0px';} 
-	    else {this.style.height = (this.scrollHeight-20)+'px';}
+	    else {this.style.height = (this.scrollHeight)+'px';}
     });
 });
