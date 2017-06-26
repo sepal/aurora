e76e80442687745d305bb407da63a94f8f424bd7
peterpur <peterpur@e245-089.eduroam.tuwien.ac.at>
2017-06-26 09:57:19 +0200

    change lock period to 7 days for last week

--- Challenge/models.py
+++ Challenge/models.py
@@ -318,7 +318,7 @@ class Challenge(models.Model):
         return result
 
     def is_in_lock_period(self, user, course):
-        PERIOD = 1
+        PERIOD = 7
         START_YEAR = 2017
         START_MONTH = 3
         START_DAY = 1
