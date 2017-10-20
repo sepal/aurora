cb5e1e4a41caf3df03d09d7c8f9dfd35556cbdfe
Daniel Domberger <queltos@gmail.com>
2017-10-20 20:16:56 +0200

    fix (Chat): initialize usernames on `ChatMessage`s

--- Chat/frontend/src/components/message_list.js
+++ Chat/frontend/src/components/message_list.js
@@ -3,7 +3,8 @@ import ChatMessage from './chat_message'
 import PropTypes from 'prop-types'
 
 const MessageList = ({messages}) => {
-  const messageList = messages.map((message, i) => <ChatMessage key={i} message={message.text}/>)
+  const messageList = messages.map((message, i) =>
+    <ChatMessage key={i} message={message.text} username={message.username}/>)
 
   return (<div>{messageList}</div>)
 }
