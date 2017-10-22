e2ecffde6228b385d1126c88a2edc75b2475150a
Daniel Domberger <queltos@gmail.com>
2017-10-22 17:49:46 +0200

    chore (Chat): remove unneeded comments

--- Chat/frontend/src/components/chat_message.js
+++ Chat/frontend/src/components/chat_message.js
@@ -1,38 +1,22 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
-// ##### ES6 Classes Version #####
-//
-// class ChatMessage extends React.Component {
-//   render() {
-//     const {message, upvotes} = this.props
-//     return (
-//       <p>{upvotes}: {message}</p>
-//     )
-//   }
-// }
-//
-// ChatMessage.propTypes = {
-//   message: PropTypes.string.isRequired,
-//   upvotes: PropTypes.int
-// }
-//
-// ChatMessage.defaultProps = {
-//   upvotes: 0
-// }
-//
-// ##############################
 
-
-// ##### Stateless functional component version #####
-const ChatMessage = ({message, username}) => {
-  return (<p>{username}: {message}</p>)
+const ChatMessage = ({message, username, staff}) => {
+  return (
+    <div>
+      <div>
+        <p className={'ChatMessageAuthor staff' + staff === 'true' ? 'staff' : ''}>{username}</p>
+      </div>
+      <div>
+        <p className='ChatMessageBody'>{message}</p>
+      </div>
+    </div>
+  )
 }
 
 ChatMessage.propTypes = {
   message: PropTypes.string.isRequired,
 }
-
-// ##################################################
 
 export default ChatMessage
