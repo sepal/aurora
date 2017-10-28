53bb289755633508af34c3586289616ba32199cb
Daniel Domberger <queltos@gmail.com>
2017-10-28 16:06:28 +0200

    chore(Chat): add built frontend to `STATICFILES_DIRS`

--- AuroraProject/settings.py
+++ AuroraProject/settings.py
@@ -98,12 +98,14 @@ STATIC_ROOT = '/vagrant/aurora/static/'
 # Example: "http://example.com/static/", "http://static.example.com/"
 STATIC_URL = '/static/'
 
+
 # Additional locations of static files
 STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
     os.path.join(os.path.dirname(PROJECT_ROOT), 'static'),
+    ('chat', os.path.join(os.path.dirname(PROJECT_ROOT), 'Chat', 'frontend', 'build', 'static'))
 )
 
 # List of finder classes that know how to find static files in
