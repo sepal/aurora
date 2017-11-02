12f3dcdd32b2db94a16ab6ac4ae9d481ee171ab6
peterpur <peterpur@vpn071.iguw.tuwien.ac.at>
2017-11-02 10:58:43 +0100

    chat now shows if people talk a lot while you're not watching, at least while you are on newsfeed page.

--- static/js/chat.js
+++ static/js/chat.js
@@ -1,11 +1,22 @@
 var chatupdate_timer_ID;
+var chat_msgs = 0;
+
+$(function(){
+	setTimeout(function() {chat_msgs = $('.MessageList').children().length; chatupdate();},1000);
+})
 
 function chatupdate()
 {
-	$('#small_talk').scrollTop($('#small_talk')[0].scrollHeight);
+	if($('#small_talk').css('display')=='block') {
+		$('#small_talk').scrollTop($('#small_talk')[0].scrollHeight);
+		$('#chat_activity').text('');	
+		chat_msgs = $('.MessageList').children().length;
+	} else {
+		newL = $('.MessageList').children().length;
+		if (newL > chat_msgs) {
+			$('#chat_activity').text('💬 '.repeat(Math.round(Math.log((newL - chat_msgs)*2))));			
+		} 
+	}
 	chatupdate_timer_ID = setTimeout(chatupdate,1000);
 }
 
-function stop_chatupdate() {
-	clearTimeout(chatupdate_timer_ID);
-}
