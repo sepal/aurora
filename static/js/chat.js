97830020f954d1b52c5d41ad2cd782d0bd2985c8
peterpur <peterpur@vpn060.iguw.tuwien.ac.at>
2017-11-05 15:29:33 +0100

    this is what chat will look like, once it works on the server...

--- static/js/chat.js
+++ static/js/chat.js
@@ -1,22 +1,36 @@
 var chatupdate_timer_ID;
 var chat_msgs = 0;
+var chat_scroll = 'Y';
 
 $(function(){
 	setTimeout(function() {chat_msgs = $('.MessageList').children().length; chatupdate();},1000);
+	chat_scroll = 'Y';
+/*	$("#small_talk").scroll(function() {
+		if (chat_scroll == 'A') {
+			chat_scroll = 'Y';
+		} else if (chat_scroll == 'M') {
+			if ($('#small_talk').scrollTop() == $('#small_talk')[0].scrollHeight) {chat_scroll = 'Y'}
+			
+		} else {
+			chat_scroll = 'M';
+		}
+	}); */
 })
 
 function chatupdate()
 {
-	if($('#small_talk').css('display')=='block') {
-		$('#small_talk').scrollTop($('#small_talk')[0].scrollHeight);
-		$('#chat_activity').text('');	
-		chat_msgs = $('.MessageList').children().length;
-	} else {
-		newL = $('.MessageList').children().length;
-		if (newL > chat_msgs) {
-			$('#chat_activity').text('💬 '.repeat(Math.round(Math.log((newL - chat_msgs)*2))));			
-		} 
+	if (chat_scroll == 'Y') {
+		if($('#small_talk').css('display')=='block') {
+			$('#small_talk').scrollTop($('#small_talk')[0].scrollHeight);
+			$('#chat_activity').text('');	
+			chat_msgs = $('.MessageList').children().length;
+		} else {
+			newL = $('.MessageList').children().length;
+			if (newL > chat_msgs) {
+				$('#chat_activity').text('💬 '.repeat(Math.round(Math.log((newL - chat_msgs)*2))));			
+			} 
+		}
 	}
-	chatupdate_timer_ID = setTimeout(chatupdate,1000);
+	chatupdate_timer_ID = setTimeout(chatupdate,2000);
 }
 
