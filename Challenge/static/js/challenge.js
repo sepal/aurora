846b5b4bc8955ea9bd1c35c3ee3e0eb63787399c
peterpur <peterpur@dhcp086.iguw.tuwien.ac.at>
2017-11-06 12:52:08 +0100

    unified stack + challenge typography and interaction

--- Challenge/static/js/challenge.js
+++ Challenge/static/js/challenge.js
@@ -30,6 +30,19 @@ $(document).ready(function() {
 
 $(challenge_loaded);
 
+function toggleDetails() {
+	$('.challenge_description').slideToggle('fast', function() {
+		xO = $('.challenge_description_title>button>i')
+		if(xO.hasClass('fa-angle-down')) {
+			xO.removeClass('fa-angle-down');
+			xO.addClass('fa-angle-up');
+		} else {
+			xO.removeClass('fa-angle-up');
+			xO.addClass('fa-angle-down');
+		}
+	});
+}
+
 function countChars() {
 	var x = trixDings.editor.getDocument().toString();
 	$('.new_word_count').text(x.replace(/^[\s,.;]+/, "").replace(/[\s,.;]+$/, "").split(/[\s,.;]+/).length);
